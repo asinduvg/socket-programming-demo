36de5640e71c29766228f1039120c0c8004af031
Asindu <ashvg.dev@gmail.com>
2022-07-14 20:03:19 +0530

    basic application finished

---
+++ client.cpp
@@ -0,0 +1,79 @@
+#include<arpa/inet.h>
+#include <cstdio>
+#include <cstring>
+#include <sys/unistd.h>
+#include <iostream>
+
+#define PORT 8080
+
+using namespace std;
+
+int main(int argc, char const* argv[]) {
+    int sock = 0, client_fd;
+    struct sockaddr_in serv_addr{};
+    char* hello = (char*)"Hello from client";
+    char buffer[1500] = {0};
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(sock < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary
+    int address_converted = inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+    if(address_converted <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    // connect to server
+    cout << "Trying to connect to server.." << endl;
+    client_fd = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+    if(client_fd < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    cout << "Connection with server succeeded!" << endl;
+
+    while(true) {
+        string data;
+        getline(cin, data);
+        memset(&buffer, 0, sizeof(buffer)); // clear the buffer
+        strcpy(buffer, data.c_str());
+
+        if(data == "quit") {
+            send(sock, (char*)&msg, strlen(buffer), 0);
+            break;
+        }
+
+        send(sock, (char*)&msg, strlen(buffer), 0);
+
+        cout << "Awaiting response..." << endl;
+
+        memset(&buffer, 0, sizeof(buffer)); // clear the buffer
+
+        // reading the server message to the buffer
+        recv(sock, (char*)&buffer, sizeof(buffer), 0);
+
+        if(!strcmp(buffer, "quit")) {
+            cout << "Server has quit the session!" << endl;
+            break;
+        }
+
+        cout << "Server says: " << buffer << endl;
+    }
+
+    // closing the connected socket
+    close(client_fd);
+    cout << "Connection closed!" << endl;
+    return 0;
+
+}
---
+++ server.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/unistd.h>
+#include <cstring>
+
+#define PORT 8080
+
+using namespace std;
+
+int main() {
+
+    int server_fd, new_socket;
+    struct sockaddr_in address{};
+    int addrlen = sizeof(address);
+    char buffer[1500] = {0};
+
+    // creating socket file descriptor
+    server_fd = socket(AF_INET, SOCK_STREAM, 0); // socket returns a descriptor referencing new socket
+
+    if(server_fd == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Forcefully attaching socket to the port 8080
+    if(bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if(listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+
+    if(new_socket < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    cout << "Client connected!" << endl;
+
+    while(true) {
+        cout << "Awaiting client response" << endl;
+        memset(&buffer, 0, sizeof(buffer)); // clear the buffer
+
+        // read client's message
+        recv(new_socket , (char*)&buffer, sizeof(buffer), 0);
+
+        if(!strcmp(buffer, "quit")) {
+            cout << "Client has quit the session!" << endl;
+            break;
+        }
+
+        cout << "Client says: " << buffer << endl;
+
+        string data;
+        getline(cin, data);
+        strcpy(buffer, data.c_str());
+
+        if(data == "quit") {
+            send(new_socket, (char*)&buffer, strlen(data.c_str()), 0);
+            break;
+        }
+
+        send(new_socket, (char*)&buffer, strlen(data.c_str()), 0);
+    }
+
+    // closing the connected socket
+    close(new_socket);
+
+    // closing the listening socket
+    shutdown(server_fd, SHUT_RDWR);
+
+    return 0;
+}
